0f1bfee06cc77545a6b480be804aa272c09c027b
“abdullah <abdullah.h.elgabry@gmail.com>
2024-04-18 00:53:16 +0200

    airbnb-v2 #done

--- models/user.py
+++ models/user.py
@@ -1,27 +1,27 @@
 #!/usr/bin/python3
-"""This is the user class"""
-from sqlalchemy.ext.declarative import declarative_base
-from models.base_model import BaseModel, Base
-from sqlalchemy import Column, Integer, String
+"""Defines the User class."""
+from models.base_model import Base
+from models.base_model import BaseModel
+from sqlalchemy import Column
+from sqlalchemy import String
 from sqlalchemy.orm import relationship
-from models.place import Place
-from models.review import Review
 
 
 class User(BaseModel, Base):
-    """This is the class for user
+    """this is a use db
     Attributes:
-        email: email address
-        password: password for you login
-        first_name: first name
-        last_name: last name
+        __tablename__ (str): db table name.
+        email: (sqlalchemy String): e-mail add.
+        password (sqlalchemy String): user pass.
+        first_name (sqlalchemy String):user fn.
+        last_name (sqlalchemy String): user ln.
+        places (sqlalchemy relationship): user rel of the place.
+        reviews (sqlalchemy relationship): user review of the place
     """
     __tablename__ = "users"
     email = Column(String(128), nullable=False)
     password = Column(String(128), nullable=False)
-    first_name = Column(String(128))
-    last_name = Column(String(128))
-    places = relationship("Place", cascade='all, delete, delete-orphan',
-                          backref="user")
-    reviews = relationship("Review", cascade='all, delete, delete-orphan',
-                           backref="user")
+    first_name = Column(String(128), nullable=True)
+    last_name = Column(String(128), nullable=True)
+    places = relationship("Place", cascade="all", backref="user")
+    reviews = relationship("Review", cascade="all", backref="user")
